d6f14bb94544db672c1f69abcbf3cec352315421
Marcelo Quesada <m.mergulhao@hotmail.com>
2023-05-23 18:27:20 -0300

    Trying to fix my shit

---
+++ Cpp testing/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    string msg[] = {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    cout<<msg[1];
+}
